c1a050de7e0acc5bd3f77d6b31a333709bd603fa
YUN TEASUNG <tingtangting9398@gmail.com>
2021-08-20 02:46:40 +0900

    1. Nginx를 이용한 무중단 배포를 위해 Profiles를 확인하는 로직 및 테스트 소스 추가 2. SecurityConfig 수정을 통해 인증없에 profile이 호출되도록 코드수정

---
+++ src/test/java/com/terry/blog/web/ProfileControllerTest.java
@@ -0,0 +1,32 @@
+package com.terry.blog.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProfileControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception{
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isEqualTo(expected);
+    }
+}
